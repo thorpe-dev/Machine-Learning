34b6cae7be38a15217d06e9a25b0c000d29ec5c5
Chris Carter <chris.carter1@ntlworld.com>
2012-02-07 11:35:46 +0000

    Adds pngs for emotion trees, forest variable saved in trees.mat

--- allRunner.m
+++ allRunner.m
@@ -1,10 +1,15 @@
 function [] = allRunner()
 
+    forest = cell(0);
+
     [examples, targets] = loaddata('cleandata_students.txt');
     for i = 1:6
         new_targets = remap(targets, i);
         tree = decisionTreeLearning(examples, (1:45), new_targets);
         DrawDecisionTree(tree)
+        forest{i} = tree;
     end
 
+    save('trees.mat', 'forest')
+   
 end
