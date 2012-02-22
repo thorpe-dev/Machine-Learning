68eb772296787c9203bf487867b410f5628e9459
Chris Carter <chris.carter1@ntlworld.com>
2012-02-22 14:10:28 +0000

    Started NN crap

---
+++ buildNNs.m
@@ -0,0 +1,40 @@
+function[] = buildNNs()
+
+[x,y] = loaddata('cleandata_students.txt');
+[x,y] = ANNdata(x,y);
+
+% create 6-output-NN
+n = 10;
+results = (1:n) * 0;
+
+for i = 1:n
+    sizes = zeros(1,i) + 6;
+    [net] = feedforwardnet(sizes);
+    [net] = configure(net, x, y);
+    
+    % would change params here
+    net.trainParam.epochs = 100;
+    net.trainParam.show = NaN;
+    net.trainParam.showWindow = 0;
+    net.trainParam.showCommandLine = 0;
+    net.trainParam.goal = 0;
+    
+    times = 100;
+    
+    for j = 1:times
+        [net] = train(net, x, y);
+        
+        [p] = sim(net, x);
+        
+        [z] = sum(round(p));
+        
+        results(i) = results(i) + size(find(z==1), 2);
+       
+    end
+    
+    results(i) = results(i)/times
+end
+
+plot((1:n), results);
+
+end
