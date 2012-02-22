2e7193fb67e2adb9a35f379a8b161b0149c3aa0b
Mark Law <cannonball88@gmail.com>
2012-02-22 15:51:23 +0000

    3dgraph

--- buildNNs.m
+++ buildNNs.m
@@ -1,40 +1,53 @@
-function[] = buildNNs()
+function[results] = buildNNs()
 
 [x,y] = loaddata('cleandata_students.txt');
 [x,y] = ANNdata(x,y);
 
 % create 6-output-NN
-n = 10;
-results = (1:n) * 0;
-
-for i = 1:n
-    sizes = zeros(1,i) + 6;
-    [net] = feedforwardnet(sizes);
-    [net] = configure(net, x, y);
-    
-    % would change params here
-    net.trainParam.epochs = 100;
-    net.trainParam.show = NaN;
-    net.trainParam.showWindow = 0;
-    net.trainParam.showCommandLine = 0;
-    net.trainParam.goal = 0;
-    
-    times = 100;
-    
-    for j = 1:times
+n = 4;
+m = 45;
+results = zeros(m,n);
+for k = 1:m
+    for i = 1:n
+        sizes = zeros(1,i) + k;
+        [net] = feedforwardnet(sizes, 'traingd');
+        [net] = configure(net, x, y);
+        
+        % would change params here
+        net.trainParam.epochs = 100;
+        net.trainParam.show = NaN;
+        net.trainParam.showWindow = 0;
+        net.trainParam.showCommandLine = 0;
+        net.trainParam.goal = 0;
+        
+        times = 10;
+        
+        for j = 1:times
         [net] = train(net, x, y);
         
         [p] = sim(net, x);
         
-        [z] = sum(round(p));
+        [z] = round(p);
         
-        results(i) = results(i) + size(find(z==1), 2);
-       
+        [u] = z & y;
+        
+        [l] = sum(u);
+
+            
+            results(k,i) = results(k,i) + size(find(l==1), 2);
+            
+        end
+        
+        results(k,i) = results(k,i)/times;
+        (((k - 1)*n+i)/(m*n)) * 100;
+        
     end
     
-    results(i) = results(i)/times
 end
 
-plot((1:n), results);
+    save('graph.mat', 'results');
 
+    %grid on;
+    
+    %surfc((1:n),(1:m), results);
 end
