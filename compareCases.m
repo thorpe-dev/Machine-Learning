ea93996bad50def4ce45e2f36766756d945f6a20
Mark Law <cannonball88@gmail.com>
2012-02-28 20:02:48 +0000

    removed dupe code

--- compareCases.m
+++ compareCases.m
@@ -4,8 +4,8 @@ function [measure] = compareCases(case1, case2)
     case1v = zeros(1, 45);
     case2v = zeros(1, 45);
 
-    case1v(case1) = 1;
-    case2v(case2) = 1;
+    case1v(case1.problem) = 1;
+    case2v(case2.problem) = 1;
 
     measure = sum(abs(case2v - case1v));
 
