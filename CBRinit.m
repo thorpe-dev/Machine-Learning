b97fa684cb2623878381545e436841d25f4d53b6
Chris Carter <chris.carter1@ntlworld.com>
2012-02-28 13:36:00 +0000

    Pushed addCase into retain, reindented

--- CBRinit.m
+++ CBRinit.m
@@ -1,31 +1,13 @@
 function [cbr] = CBRinit(x, y)
 % Given x and y, creates the CBR
 
-cbr = cell(0);
+    cbr = cell(0);
 
-for i = 1:size(x, 1)
-    case_x = find(x(i,:) == 1);
-    case_y = y(i);
-    thisCase = createCase(case_x, case_y);
-    cbr = addCase(cbr, thisCase);
-end
-
-end
-
-function [cbr] = addCase(cbr, newcase)
-% Adds a new case to the CBR, considering dupes
-
-for i = 1:size(cbr, 1)
-   if isequal(newcase.problem, cbr{i}.problem)
-      t = cbr{i}.typicality;
-      t = t + newcase.typicality;
-      cbr{1}.typicality = t;
-      %cbr = cbr;
-      return;
-   end
-end
-
-cbr{end+1} = newcase;
-%cbr = cbr;
+    for i = 1:size(x, 1)
+        case_x = find(x(i,:) == 1);
+        case_y = y(i);
+        thisCase = createCase(case_x, case_y);
+        cbr = retain(cbr, thisCase);
+    end
 
 end
--- createCase.m
+++ createCase.m
@@ -2,9 +2,9 @@ function [newCase] = createCase(AUs, emotion)
 % Given a vector of AUs and an emotion number (1-6),
 % return a case structure
 
-newCase.problem = AUs;
-t = zeros(1,6);
-t(emotion) = 1;
-newCase.typicality = t;
+    newCase.problem = AUs;
+    t = zeros(1,6);
+    t(emotion) = 1;
+    newCase.typicality = t;
 
 end
