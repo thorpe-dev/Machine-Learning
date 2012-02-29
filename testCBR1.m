e15f91994cd6fa89e4c5c9aa485b9a5389940e61
Mark Law <cannonball88@gmail.com>
2012-02-29 13:26:20 +0000

    fixed silly persons mistake

--- testCBR1.m
+++ testCBR1.m
@@ -4,7 +4,7 @@ function [y] = testCBR(CBR, x)
 
     for i = 1:size(x,1)
         
-        AUs = find(x(i) == 1);
+        AUs = find(x(i,:) == 1);
         [newcase] = createNovelCase(AUs);
         [bestcase] = retrieve(CBR, newcase);
         [solvedcase] = reuse(bestcase, newcase);
