61437fa78416f24361a8a057a94e8d1ac53526bc
Mark Law <cannonball88@gmail.com>
2012-02-29 12:56:12 +0000

    fixed a sizeable error

--- retain.m
+++ retain.m
@@ -1,9 +1,9 @@
 function [cbr] = retain(cbr, newcase)
 % Adds a new case to the CBR, considering dupes
 
-    for i = 1:size(cbr, 1)
-        if isequal(newcase.problem, cbr{i}.problem)
-            t = cbr{i}.typicality;
+    for i = 1:size(cbr, 2)
+        if compareCases(newcase, cbr{i}) == 0;
+            t = cbr{i}.typicality
             t = t + newcase.typicality;
             cbr{1}.typicality = t;
             return;
--- retrieve.m
+++ retrieve.m
@@ -15,8 +15,7 @@ function [c] = retrieve(cbr, newcase)
         cases{1} = current;
       end
     end
-
-    max_typ = cases{1}.typicality;
+    max_typ = sum(cases{1}.typicality);
     typ_cases = cell(0);
     typ_cases{1} = cases{1};
     for i = 2:size(cases,2)
@@ -30,10 +29,10 @@ function [c] = retrieve(cbr, newcase)
         typ_cases{1} = current;
       end
     end
+    typ_cases
     c = typ_cases{randi(size(typ_cases,2))};
 
   end
-
 end
 
 
