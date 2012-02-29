6fd098667571f4533832f91163d433b7ff3ce4a9
Mark Law <cannonball88@gmail.com>
2012-02-29 12:35:52 +0000

    made retrieve take most typical

--- retrieve.m
+++ retrieve.m
@@ -15,8 +15,22 @@ function [c] = retrieve(cbr, newcase)
         cases{1} = current;
       end
     end
-
-    c = cases{randi(size(cases,2))};
+    
+    max_typ = cases{1}.typicality;
+    typ_cases = cell(0);
+    typ_cases{1} = cases{1};
+    for i = 2:size(cases,2)
+      current = cases{i};
+      typ = current.typicality;
+      if(max_typ == typ)
+        typ_cases{end + 1} = current;
+      elseif(max_typ < typ)
+        max_typ = typ;
+        typ_cases = {};
+        typ_cases{1} = current;
+      end
+    end
+    c = typ_cases{randi(size(typ_cases,2))};
 
   end
 
