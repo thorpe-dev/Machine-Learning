2c7a1a660bf2892988cefbf8245348991af145b2
Chris Carter <chris.carter1@ntlworld.com>
2012-03-14 19:45:26 +0000

    Added function to extract F1 data

---
+++ extractF1Data.m
@@ -0,0 +1,18 @@
+function [f1s] = extractF1Data()
+
+% Extract the data for the ANOVA tests
+
+f1s = cell(0);
+
+% Decision Tree data
+f1s{1} = runner();
+
+% NNs data
+load('data/networks.mat');
+f1s{2} = testNNs(networks);
+
+% CBR data
+f1s{3} = testCBRs(2,7);
+
+end
+
