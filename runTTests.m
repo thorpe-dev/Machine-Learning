7c14b236e291bc56b6e24968bbf660c59d5933b5
Chris Carter <chris.carter1@ntlworld.com>
2012-03-15 14:00:29 +0000

    Fixed all the boll...KILL THEM

--- runTTests.m
+++ runTTests.m
@@ -6,7 +6,7 @@ tData = zeros(6,3);
 
 for i = 1:6
    for j = 1:3
-     tData(i,j) = ttest2(emotionF1sNoisy{i}(:,j), emotionF1sNoisy{i}(:,mod(j,3) + 1), 0.05);
+     tData(i,j) = ttest2(emotionF1sNoisy{i}(:,j), emotionF1sNoisy{i}(:,mod(j,3) + 1) + eps, 0.05);
    end
 end
 
